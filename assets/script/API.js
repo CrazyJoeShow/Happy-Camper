e1adb22cee1c2fa6e6b023c445fdcbdecb0ed7f3
Joe Lowery <joseph.loweryit@gmail.com>
2020-09-18 12:50:20 -0400

    created the API.js file

---
+++ assets/script/API.js
@@ -0,0 +1,12 @@
+// By ZIP code
+// Description:
+// Please note if country is not specified then the search works for USA as a default.
+
+// API call:
+// api.openweathermap.org/data/2.5/forecast?zip={zip code},{country code}&appid={your api key}
+// Parameters:
+// zip zip code
+
+document.ready(function) () {
+	console.log('the document is ready');
+};
